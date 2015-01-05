45bcc763591296a6d10eea75d016911da95b6fed
kamasu <kamasu@kamasu.jp>
2015-01-05 14:52:36 +0900

    add path select

--- src/name/gntm_mdk/RecordMain.java
+++ src/name/gntm_mdk/RecordMain.java
@@ -2,6 +2,7 @@ package name.gntm_mdk;
 import java.awt.Button;
 import java.awt.Checkbox;
 import java.awt.Choice;
+import java.awt.FileDialog;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
@@ -14,32 +15,37 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class RecordMain extends Frame implements ActionListener{
-	final private String SETTING_FILE_NAME = "settings.conf";
-	
+	private static final String DEFAULT_FILE_NAME = "record.wav";
+	private static final String SETTING_FILE_NAME = "property.conf";
+	private static final String SETTING_KEY_URI = "setting_key_uri";
+	private static final String SETTING_KEY_TYPE = "setting_key_type";
+
 	private Recorder mRecorder;
 	private Thread mThread;
 	Label mHourLabel;
 	Label mMinuteLabel;
+	Label mPathLabel;
 	Choice mMinuteChoice;
 	Choice mHourChoice;
 	boolean mIsImmediate = false;
-	
+
 	private Properties prop;
-	
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Recorder recorder = new Recorder();
 		RecordMain record = new RecordMain();
 		record.setVisible(true);
 	}
@@ -47,21 +53,21 @@ public class RecordMain extends Frame implements ActionListener{
 	public RecordMain(){
 		// Window initialization
 		super();
+		// load default settings
+		prop = new Properties();
+		mRecorder = load();
+
 		//ウィンドウを閉じる時
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-		setSize(300, 300);
+		setSize(600, 200);
 		buildUi();
 		// other initialization
-		mRecorder = new Recorder();
 		mThread = new Thread();
 
-		// load default settings
-		prop = new Properties();
-		load();
 	}
 
 	@Override
@@ -97,9 +103,24 @@ public class RecordMain extends Frame implements ActionListener{
 		Label lineLabel = new Label("Line:");
 		add(lineLabel);
 		Choice lineChoice = new Choice();
-		lineChoice.add("LINE_OUT");
-		lineChoice.add("SPEAKER");
-		lineChoice.add("Line");
+		final ArrayList<String> list = new ArrayList<String>();
+		list.add("LINE_OUT");
+		list.add("SPEAKER");
+		list.add("Line");
+		for (String s : list){
+			lineChoice.add(s);
+		}
+		lineChoice.select(Integer.parseInt(prop.getProperty(SETTING_KEY_TYPE)));
+		lineChoice.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				// TODO Auto-generated method stub
+				int order =  list.indexOf(e.getItem().toString());
+				System.out.println("selected :" + order);
+				prop.setProperty(SETTING_KEY_TYPE, order+"");
+				save();
+			}
+		});
 		add(lineChoice);
 
 		mHourLabel = new Label("hour:");
@@ -128,12 +149,25 @@ public class RecordMain extends Frame implements ActionListener{
 		});
 		add(checkBox);
 
+
+		mPathLabel = new Label("保存先:"+prop.getProperty(SETTING_KEY_URI));
+		add(mPathLabel);
+		Button srcButton = new Button("保存先を変更");
+		srcButton.setSize(300, 50);
+		srcButton.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				selectURI();
+			}
+		});
+		add(srcButton);
+
 		Button recordButton = new Button("Rec");
 		recordButton.setSize(300, 50);
 		recordButton.addActionListener(new ActionListener() {			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-
+				selectURI();
 			}
 		});
 		add(recordButton);
@@ -155,14 +189,28 @@ public class RecordMain extends Frame implements ActionListener{
 		mMinuteChoice.setEnabled(!isChecked);
 		mIsImmediate = isChecked;
 	}
-	
-	
+
+
 	/*
 	 * util
 	 */
 
+	private void selectURI(){
+		FileDialog fileDialog=new FileDialog(this , "保存先を選択" , FileDialog.SAVE );//FileDialogの作成
+		fileDialog.setVisible(true);//表示する
+		String dir=fileDialog.getDirectory();//ディレクトリーの取得
+		String fileName=fileDialog.getFile();//File名の取得
+		if (fileName!=null){
+			String path = dir + fileName;
+			System.out.println(path);
+			prop.setProperty(SETTING_KEY_URI, path );
+			mPathLabel.setText("保存先:"+path);
+			save();
+		}
+	}
+
 	private void save(){
-        try {
+		try {
 			prop.store(new FileOutputStream(SETTING_FILE_NAME), "settings");
 		} catch (FileNotFoundException e1) {
 			System.out.println("save failed : not found");
@@ -170,13 +218,27 @@ public class RecordMain extends Frame implements ActionListener{
 			System.out.println("save failed : IO blocked");
 		}
 	}
-	private void load(){
+	private Recorder load(){
 		try {
 			prop.load(new FileInputStream(SETTING_FILE_NAME));
 		} catch (IOException e) {
-			System.out.println("not found");
+			System.out.println("setting is not found.");
+
+			String path = new File(".").getAbsoluteFile().getParent() + "/"+DEFAULT_FILE_NAME;
+			System.out.println("default file path :"+path);			
+			prop.setProperty(SETTING_KEY_URI, path);		
+			prop.setProperty(SETTING_KEY_TYPE, Recorder.LINE_TYPE.SPEAKER.ordinal()+"");
 			save();
 		}
+		Recorder r = new Recorder(
+				prop.getProperty(SETTING_KEY_URI) ,
+				fromOrdinal(Recorder.LINE_TYPE.class, Integer.parseInt(prop.getProperty(SETTING_KEY_TYPE))));
+		return r;
+	}
+
+	public static <E extends Enum<E>> E fromOrdinal(Class<E> enumClass, int ordinal) {
+		E[] enumArray = enumClass.getEnumConstants();
+		return enumArray[ordinal];
 	}
 
 }
